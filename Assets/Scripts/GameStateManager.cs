b9c14ad01b33bc1d195e60c56b85626c03ba87ef
jhalko <jaayden.halko@am.sony.com>
2015-01-24 13:45:39 -0800

    new background

---
+++ Assets/Scripts/GameStateManager.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameStateManager : MonoBehaviour {
+
+	public static int StartingScore = 0;
+	private int score;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
