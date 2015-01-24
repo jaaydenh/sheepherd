c767c2635767f4284335d3ecead6bf7781e2d179
jhalko <jaayden.halko@am.sony.com>
2015-01-24 11:02:18 -0800

    initial commit

---
+++ Assets/Scripts/GhostMove.cs
@@ -0,0 +1,74 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GhostMove : MonoBehaviour {
+	public Transform[] waypoints;
+	int cur = 0;
+	
+	public GameObject[] herders;
+	public GameObject[] sheep;
+	
+	public float fearError = 0.1f;
+	public float fearConstant = 1f;
+	public float flockError = .1f;
+	public float flockConstant = 1f;
+	
+	public float fearScale = 1.0f;
+	public float flockScale = 0.25f;
+	
+	void Start() {
+		herders = GameObject.FindGameObjectsWithTag("herder");
+		sheep = GameObject.FindGameObjectsWithTag("sheep");
+	}
+	
+	public float speed = 0.01f;
+	
+	// if we had done this right - applying forces to physics objects - it would have been easier to implement
+	// http://gamedevelopment.tutsplus.com/series/understanding-steering-behaviors--gamedev-12732
+	
+	void FixedUpdate () {	
+		Vector2 fearVector = Vector2.zero; // run away from the herders * their closeness
+		foreach(GameObject h in herders) {
+			float dx = transform.position.x - h.transform.position.x;
+			float dy = transform.position.y - h.transform.position.y;
+			
+			// get a unit vector pointing in the right direction
+			Vector2 sheepToHerder = new Vector2(dx, dy); sheepToHerder /= sheepToHerder.magnitude;
+			
+			// scale it proportional to 1/distance^2, but make sure we never deal with distance=0
+			float useDistance = Mathf.Max (sheepToHerder.magnitude, fearError);
+			float closeness = fearConstant * (1/ (useDistance * useDistance));
+			sheepToHerder *= closeness;
+			
+			// and add it to the running sum
+			fearVector += (sheepToHerder * fearScale);
+		}
+		
+		Vector2 flockVector = Vector2.zero; // run toward the centroid of the sheep / closeness
+		Vector2 centroid = Vector2.zero;
+		foreach(GameObject s in sheep) {
+			centroid += (Vector2)s.transform.position;
+			
+		}
+		centroid /= sheep.Length; // where the average sheep is
+		
+		// now take a path toward the centroid, but stop if we're close enough (<error)
+		Vector2 sheepToCentroid = centroid - (Vector2)transform.position;
+		if(sheepToCentroid.magnitude > flockError) {
+			float contentness = flockConstant * (1 / Mathf.Max (sheepToCentroid.magnitude, flockError));
+			flockVector = sheepToCentroid * contentness;
+		}
+		
+		Vector2 netVector = flockVector + fearVector;
+		
+		Vector2 target = Vector2.MoveTowards(transform.position, (Vector2)transform.position + netVector, speed);
+		
+		rigidbody2D.MovePosition(target);
+		
+		// Animation
+		//		Vector2 dir = netVector;
+		//        GetComponent<Animator>().SetFloat("DirX", dir.x);
+		//        GetComponent<Animator>().SetFloat("DirY", dir.y);
+	}
+	
+}
---
+++ Assets/Scripts/PacmanMove.cs
@@ -0,0 +1,106 @@
+using UnityEngine;
+using System.Collections;
+
+public class PacmanMove : MonoBehaviour {
+    public float speed = 0.4f;
+    Vector2 dest = Vector2.zero;
+
+	public KeyCode upKey, downKey, leftKey, rightKey;
+	int sn = 0;
+
+    void Start() {
+        dest = transform.position;
+		/* upKey = KeyCode.UpArrow;
+		downKey = KeyCode.DownArrow;
+		leftKey = KeyCode.LeftArrow;
+		rightKey = KeyCode.RightArrow; */
+	}
+
+	void OnCollisionEnter2D() {
+		//print ("OnCollisionEnter2D");
+		//dest = (Vector2)transform.position;
+	}
+
+	void OnCollisionStay2D() {
+
+//		if ((Vector2)transform.position != dest) {
+//			print ("OnCollisionStay2D");
+//			dest = (Vector2)transform.position;
+//		}
+	}
+
+    void FixedUpdate() {
+		//print (++sn + " " + gameObject.name);
+		//print (++sn + " Pacman fixedupdate");
+        // Move closer to Destination
+        Vector2 p = Vector2.MoveTowards(transform.position, dest, speed);
+        rigidbody2D.MovePosition(p);
+		float horizontal = 0;
+		float vertical = 0;
+		if (gameObject.name == "pacman1") {
+			horizontal = Input.GetAxis ("HorizontalP1");
+			vertical = Input.GetAxis ("VerticalP1");
+		} else if (gameObject.name == "pacman2"){
+			horizontal = Input.GetAxis ("HorizontalP2");
+			vertical = Input.GetAxis ("VerticalP2");
+		}
+		//print ("axis6: " + axis6);
+		//print ("axis7: " + axis7);
+		// print ("input.getkey: " + Input.GetKey(upKey));
+
+        // Check for Input if not moving
+       // if ((Vector2)transform.position == dest) {
+
+//		if (Input.GetAxis(upKey) && valid (Vector2.up)) {
+//			dest = (Vector2)transform.position + Vector2.up;
+//		}
+		Vector2 input = new Vector2 ();
+		input.x = horizontal;
+		input.y = vertical;
+
+		dest = (Vector2)transform.position + input;
+
+//		if (vertical > 0 && valid (Vector2.up)) {
+//			dest = (Vector2)transform.position + Vector2.up;
+//		}
+//		if (vertical < 0 && valid (-Vector2.up)) {
+//			dest = (Vector2)transform.position - Vector2.up;
+//		}
+//		if (horizontal > 0 && valid (Vector2.right)) {
+//			dest = (Vector2)transform.position + Vector2.right;
+//		}
+//		if (horizontal < 0 && valid (-Vector2.right)) {
+//			dest = (Vector2)transform.position - Vector2.right;
+//		}
+//			if (Input.GetKey (upKey) && valid (Vector2.up)) {
+//				dest = (Vector2)transform.position + Vector2.up;
+//			}
+//			if (Input.GetKey (rightKey) && valid (Vector2.right)) {
+//				dest = (Vector2)transform.position + Vector2.right;
+//			}
+//			if (Input.GetKey (downKey) && valid (-Vector2.up)) {
+//				dest = (Vector2)transform.position - Vector2.up;
+//			}
+//			if (Input.GetKey (leftKey) && valid (-Vector2.right)) {
+//				dest = (Vector2)transform.position - Vector2.right;
+//			}
+							// else print(++sn + " no input");
+				//} else {
+			//dest = transform.position;
+		//}
+
+        // Animation Parameters
+        Vector2 dir = dest - (Vector2)transform.position;
+        GetComponent<Animator>().SetFloat("DirX", dir.x);
+        GetComponent<Animator>().SetFloat("DirY", dir.y);
+    }
+
+    bool valid(Vector2 dir) {
+        // Cast Line from 'next to Pac-Man' to 'Pac-Man'
+		// return true;
+        Vector2 pos = transform.position;
+        RaycastHit2D hit = Physics2D.Linecast(pos + dir, pos);
+		//if (hit.collider != collider2D) print (gameObject.name + " collision detected, no move");
+        return (hit.collider == collider2D);
+    }
+}
