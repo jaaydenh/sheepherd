7d6f2432953cb8f481c8fa4799bfe0f12e0178e1
Colin Creitz <ccreitz@fb.com>
2015-01-24 16:46:52 -0800

    Added skeletal timer code. In GameStateManager, set the public property levelTime to the number of seconds you want to run (default 60), set runTimer to true to start the clock, and the function EndLevel will be called when time runs out.

--- Assets/Scripts/GameStateManager.cs
+++ Assets/Scripts/GameStateManager.cs
@@ -6,6 +6,9 @@ public class GameStateManager : MonoBehaviour {
 	public static int StartingScore = 0;
 	private int score;
 
+	public int levelTime = 60; // in s
+	public bool runTimer = false;
+
 	// Use this for initialization
 	void Start () {
 	
@@ -13,6 +16,26 @@ public class GameStateManager : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-	
+		if(runTimer) {
+			levelTime -= Time.deltaTime;
+			if (levelTime <= 0) {
+				EndLevel("fail");
+			}
+		}
 	}
+
+	void OnGUI () {
+		// see https://github.com/fbsamples/friendsmash-unity/blob/master/friendsmash_complete/Assets/Scripts/GameStateDisplay.cs
+		// for how to display the time remaining, if you want to do it with a GUI. Else manipulate a sprite in Update().
+	}
+
+	public void EndLevel(string result) {
+		if (result == "fail") {
+
+		} else if (result == "succeed") {
+
+		}
+	}
+
+
 }
